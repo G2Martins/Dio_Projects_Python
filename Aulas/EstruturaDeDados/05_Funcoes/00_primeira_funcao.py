33ceee1cff1654e362dd7133681b1f0d345a72d3
G2Martins <gustavogripaldi@outlook.com>
2023-10-09 20:58:45 -0300

    add ffuncao files

---
+++ Aulas/EstruturaDeDados/05_Funcoes/00_primeira_funcao.py
@@ -0,0 +1,16 @@
+def exibir_mensagem():
+    print("Hello Word")
+
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+
+def exibir_mensagem_3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}!")
+
+
+exibir_mensagem()
+exibir_mensagem_2(nome="Guilherme")
+exibir_mensagem_3()
+exibir_mensagem_3(nome="Chappie")
