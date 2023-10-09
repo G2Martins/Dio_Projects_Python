ccb5b7b22b52cf499cbc1f570708a6575eb2a4c8
G2Martins <gustavogripaldi@outlook.com>
2023-10-09 01:34:42 -0300

    docs: add files Tuplas

--- Aulas/EstruturaDeDados/01_Listas/16_sorted.py
+++ Aulas/EstruturaDeDados/01_Listas/16_sorted.py
@@ -1,7 +1,9 @@
 linguagens = ["python", "js", "c", "java", "csharp"]
 
+# Ordenação pelo Tamanho da palavra Ascendentemente
 print(sorted(linguagens, key=lambda x: len(x)))
 # ["c", "js", "java", "python", "csharp"]
 
+# Ordenação pelo Tamanho da palavra Decrescentemente
 print(sorted(linguagens, key=lambda x: len(x), reverse=True))
 # ["python", "csharp", "java", "js", "c"]
