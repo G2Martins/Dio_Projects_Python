165d559f5606a832295ad449402667e9e5416a03
G2Martins <gustavogripaldi@outlook.com>
2023-10-19 00:33:37 -0300

    update files SQL Alchemy

--- Aulas/SQLAlchemy/main.py
+++ Aulas/SQLAlchemy/main.py
@@ -1,4 +1,37 @@
-import sqlalchemy
-import pymongo
+from pickle import TRUE
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 
-print('Hello')
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user_account"
+    
+    # Atributos
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+
+    # Relacionamento
+    adress = relationship(
+        "Address", back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"User(id={self.id}, name={self.name}, fullname={self.fullname})"
+
+class Adress(Base):
+    __tablename__ = "address"
+
+    # Atributos
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String(40), nullable=False)                           # Obrigatório
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)     # Obrigatório
+
+    user = relationship("User", back_populates="address")
+    
+    def __repr__(self):
+        return f"Address (id={self.id}, email={self.email_address})"
+
+print(User.__tablename__)
+print(User.__table__)
